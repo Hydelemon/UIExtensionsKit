f5ea9f15831d12597f5c2b22c7758ec80ecb52ed
hydes <hydelemon@yandex.ru>
2024-03-28 13:34:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/hydelemon/uiextensionskit/EnableViewGroupUtils.kt
@@ -0,0 +1,18 @@
+package ru.hydelemon.uiextensionskit
+
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+class EnableViewGroupUtils {
+    fun enableDisableView(view: View, enabled: Boolean) {
+        view.isEnabled = enabled
+        if (view is ViewGroup) {
+            for (idx in 0 until view.childCount) {
+                enableDisableView(view.getChildAt(idx), enabled)
+            }
+        }
+    }
+}
